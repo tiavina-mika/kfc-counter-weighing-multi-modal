8817fd9fa8e98ebef7f2d3c2eac0000dade7b470
tiavina-mika <tiavinamika@gmail.com>
2025-02-13 16:53:16 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import RecipeDialogForm from './containers/RecipeDialogForm';
 
 const App = () => {
+  const [isOpenRecipeDialog, setIsOpenRecipeDialog] = useState(false)
+  const [selectedRecipe, setSelectedRecipe] = useState<Record<string, any> | null>(null)
+
+  const toggleOpenRecipeDialog = () => setIsOpenRecipeDialog(!isOpenRecipeDialog)
+
+  const handleSelectRecipe = (recipe: Record<string, any>) => {
+    console.log('recipe: ', recipe);
+    return
+    setSelectedRecipe(recipe)
+    toggleOpenRecipeDialog()
+  }
+
   return (
     <Stack spacing={2} direction="row">
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
+      <Button onClick={toggleOpenRecipeDialog}>Faire une contre-pesée/reprod</Button>
+
+      <RecipeDialogForm
+        onClose={toggleOpenRecipeDialog}
+        open={isOpenRecipeDialog}
+        onSubmit={handleSelectRecipe}
+      />
     </Stack>
   );
 }
