2833354d5c59dd9f1a83aacd1467aa58f6adc25a
tiavina-mika <tiavinamika@gmail.com>
2025-02-13 17:32:48 +0300

    Create App.tsx

---
+++ code/src/App.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import RecipeDialogForm from '../../src/containers/RecipeDialogForm';
+
+const App = () => {
+  const [isOpenRecipeDialog, setIsOpenRecipeDialog] = useState(false)
+  const [selectedRecipe, setSelectedRecipe] = useState<Record<string, any> | null>(null)
+
+  const toggleOpenRecipeDialog = () => setIsOpenRecipeDialog(!isOpenRecipeDialog)
+
+  const handleSelectRecipe = (recipe: Record<string, any>) => {
+    console.log('recipe: ', recipe);
+    return
+    setSelectedRecipe(recipe)
+    toggleOpenRecipeDialog()
+  }
+
+  return (
+    <Stack spacing={2} direction="row">
+      <Button onClick={toggleOpenRecipeDialog}>Faire une contre-pesée/reprod</Button>
+
+      <RecipeDialogForm
+        onClose={toggleOpenRecipeDialog}
+        open={isOpenRecipeDialog}
+        onSubmit={handleSelectRecipe}
+      />
+    </Stack>
+  );
+}
+
+export default App
