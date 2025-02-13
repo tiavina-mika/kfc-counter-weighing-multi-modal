72bf15ccfa8b9af6773ec7e5eb20d2465f0d1862
tiavina-mika <tiavinamika@gmail.com>
2025-02-13 16:53:31 +0300

    Create data.ts

---
+++ src/utils/data.ts
@@ -0,0 +1,130 @@
+export const packagingExecutions = [
+  {
+    recipe: {
+      objectId: 'recipe1',
+      uniqueCode: 'RCP1',
+      sections: [
+        {
+          objectId: 'section11',
+          name: 'Section 11',
+        },
+        {
+          objectId: 'section12',
+          name: 'Section 12',
+        }
+      ]
+    }
+  },
+  {
+    recipe: {
+      objectId: 'recipe2',
+      uniqueCode: 'RCP2',
+      sections: [
+        {
+          objectId: 'section21',
+          name: 'Section 21',
+        },
+        {
+          objectId: 'section22',
+          name: 'Section 22',
+        }
+      ]
+    }
+  },
+  {
+    recipe: {
+      objectId: 'recipe3',
+      uniqueCode: 'RCP3',
+      sections: [
+        {
+          objectId: 'section31',
+          name: 'Section 31',
+        },
+        {
+          objectId: 'section32',
+          name: 'Section 32',
+        }
+      ]
+    }
+  },
+  {
+    recipe: {
+      objectId: 'recipe4',
+      uniqueCode: 'RCP4',
+      sections: [
+        {
+          objectId: 'section41',
+          name: 'Section 41',
+        },
+        {
+          objectId: 'section42',
+          name: 'Section 42',
+        }
+      ]
+    }
+  },
+  {
+    recipe: {
+      objectId: 'recipe5',
+      uniqueCode: 'RCP5',
+      sections: [
+        {
+          objectId: 'section51',
+          name: 'Section 51',
+        },
+        {
+          objectId: 'section52',
+          name: 'Section 52',
+        }
+      ]
+    }
+  },
+  {
+    recipe: {
+      objectId: 'recipe6',
+      uniqueCode: 'RCP6',
+      sections: [
+        {
+          objectId: 'section61',
+          name: 'Section 61',
+        },
+        {
+          objectId: 'section62',
+          name: 'Section 62',
+        }
+      ]
+    }
+  },
+  {
+    recipe: {
+      objectId: 'recipe7',
+      uniqueCode: 'RCP7',
+      sections: [
+        {
+          objectId: 'section71',
+          name: 'Section 71',
+        },
+        {
+          objectId: 'section72',
+          name: 'Section 72',
+        }
+      ]
+    }
+  },
+  {
+    recipe: {
+      objectId: 'recipe8',
+      uniqueCode: 'RCP8',
+      sections: [
+        {
+          objectId: 'section81',
+          name: 'Section 81',
+        },
+        {
+          objectId: 'section82',
+          name: 'Section 82',
+        }
+      ]
+    }
+  },
+]
