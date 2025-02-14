c323d1f7e05ff77f77b8c0b5347eaca6341b8597
tiavina-mika <tiavinamika@gmail.com>
2025-02-14 15:39:25 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ const App = () => {
   return (
     <Box sx={{ minHeight: '100vh', display: 'flex', flexDirection: "column", flexGrow: 1 }}>
       <CssBaseline />
+      {/* app bar */}
       <AppBar position="static" component="nav" sx={{ bgcolor: '#000' }}>
         <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
           <Box sx={{ width: '140px' }}>
@@ -30,12 +31,14 @@ const App = () => {
           </Box>
         </Toolbar>
       </AppBar>
+      {/* page title */}
       <Typography
         variant="h6"
         sx={{ ml: 4, mt: 4, display: 'flex', justifyContent: 'center', alignItems: 'center' }}
       >
         Ouvrir la série des modales de contre-pesée
       </Typography>
+      {/* content */}
       <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
         <Button onClick={toggleOpenRecipeDialog} variant="contained">Faire une contre-pesée/reprod</Button>
 
