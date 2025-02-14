01dcecdd01d22420697fd8baf0ba7549cd4c2a6a
tiavina-mika <tiavinamika@gmail.com>
2025-02-14 15:54:07 +0300

    Update Recipes.tsx

--- src/containers/Recipes.tsx
+++ src/containers/Recipes.tsx
@@ -1,8 +1,13 @@
-import { Table, TableBody, TableCell, TableHead, TableRow } from '@mui/material';
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material';
 import { packagingExecutions } from '../utils/data';
 
 const Recipes = () => {
   return (
+    <Paper sx={{ width: '100%', mb: 2 }}>
+      <Typography variant="h6" component="h2" sx={{ p: 2 }}>
+        Liste des recettes à rechercher pour la contre-pesée
+      </Typography>
+      <TableContainer>
         <Table>
           <TableHead>
             <TableRow>
@@ -16,7 +21,10 @@ const Recipes = () => {
               </TableRow>
             ))}
           </TableBody>
-        </Table>
+        </Table>       
+      </TableContainer>
+
+    </Paper>
   );
 }
 
