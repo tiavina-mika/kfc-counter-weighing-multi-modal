2e7c599fa05294abe38238ce0669367c3651f357
tiavina-mika <tiavinamika@gmail.com>
2025-02-17 16:09:43 +0300

    Update utils.ts

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,19 +1,13 @@
 import { packagingExecutions } from "./data"
 
+/**
+ * Mock database search for recipes by unique code or name
+ * @param search 
+ * @returns 
+ */
 export const searchRecipesByUniqueCodeOrName = (search: string) => {
   const recipes = packagingExecutions.map((packagingExecution: Record<string, any>) => packagingExecution.recipe)
   return recipes.filter((recipe: Record<string, any>) => {
     return recipe.uniqueCode.toLowerCase().includes(search.toLowerCase()) || recipe.name.toLowerCase().includes(search.toLowerCase())
-  })
-}
-
-export const formatPECounterWeighingSectionsInitialValues = (packagingExecution: Record<string, any> | null) => {
-  const sections = packagingExecution?.sections || []
-  return sections.map((section: Record<string, any>) => {
-    if (section.counterWeighing) {
-      return { ...section, reason: section.counterWeighing.reason, weight: section.counterWeighing.weight || 0 }
-    }
-
-    return section
   })
 }
