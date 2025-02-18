e50cacb8b7ab24552b8fc632adcf630f9c55dd06
tiavina-mika <tiavinamika@gmail.com>
2025-02-18 16:38:13 +0300

    Update utils.ts

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -3,9 +3,8 @@
  * @param search 
  * @returns 
  */
-export const searchRecipesByUniqueCodeOrName = (packagingExecutions: Record<string, any> = [], search: string) => {
-  const recipes = packagingExecutions.map((packagingExecution: Record<string, any>) => packagingExecution.recipe)
-  return recipes.filter((recipe: Record<string, any>) => {
-    return recipe.uniqueCode.toLowerCase().includes(search.toLowerCase()) || recipe.name.toLowerCase().includes(search.toLowerCase())
+export const searchRecipesByUniqueCodeOrName = (packagingExecutions: Record<string, any>[] = [], search: string) => {
+  return packagingExecutions.filter((pe) => {
+    return pe.uniqueCode.toLowerCase().includes(search.toLowerCase()) || pe.recipeName.toLowerCase().includes(search.toLowerCase())
   })
 }
