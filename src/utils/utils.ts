bab56923e8c627c60e6382141054543e24dc2daf
tiavina-mika <tiavinamika@gmail.com>
2025-02-18 15:58:13 +0300

    Update utils.ts

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,11 +1,9 @@
-import { packagingExecutions } from "./data"
-
 /**
  * Mock database search for recipes by unique code or name
  * @param search 
  * @returns 
  */
-export const searchRecipesByUniqueCodeOrName = (search: string) => {
+export const searchRecipesByUniqueCodeOrName = (packagingExecutions: Record<string, any> = [], search: string) => {
   const recipes = packagingExecutions.map((packagingExecution: Record<string, any>) => packagingExecution.recipe)
   return recipes.filter((recipe: Record<string, any>) => {
     return recipe.uniqueCode.toLowerCase().includes(search.toLowerCase()) || recipe.name.toLowerCase().includes(search.toLowerCase())
