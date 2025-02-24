d21a565100dcba3a385d29161f33e5258a15dd4e
tiavina-mika <tiavinamika@gmail.com>
2025-02-24 22:28:31 +0300

    Update SectionField.tsx

--- src/containers/SectionField.tsx
+++ src/containers/SectionField.tsx
@@ -1,13 +1,12 @@
 import Typography from '@mui/material/Typography';
-import { Box, Button, Card, Stack, styled, TextField } from '@mui/material';
-import { ChangeEvent } from 'react';
+import { Button, Card, Stack, styled } from '@mui/material';
 import { FormikErrors } from 'formik';
 import { COLORS } from '../utils/constants';
+import WeightInput from './WeightInput';
 
 const primaryColor = COLORS.primary
 const errorColor = COLORS.error
 const activeColor = COLORS.active
-const grayColor = COLORS.gray
 
 const StyleCard = styled(Card, {
   shouldForwardProp: (prop) => prop !== "isSelected" && prop !== "hasError"
@@ -41,116 +40,10 @@ const StyleCard = styled(Card, {
   return styles
 })
 
-const StyledWeightInput = styled(TextField, {
-  shouldForwardProp: (prop) => prop !== "isPositiveNumber"
-})(({ isPositiveNumber = false }: { isPositiveNumber: boolean }) => {
-  const styles: Record<string, any> = {
-    flex: 1,
-    padding: 0,
-    '& .MuiOutlinedInput-notchedOutline': {
-      textAlign: 'center',
-      border: 'none'
-    },
-    '& .MuiInputBase-input': {
-      fontSize: '40px',
-      fontStyle: 'normal',
-      fontWeight: 500,
-      lineHeight: '120%',
-      padding: 0,
-      minWidth: 48,
-      textAlign: 'center',
-      display: 'flex',
-      justifyContent: 'center',
-      // remove arrows for chrome, safari, edge
-      '&::-webkit-outer-spin-button, &::-webkit-inner-spin-button': {
-        '-webkit-appearance': 'none',
-        margin: 0
-      },
-      // remove arrows for firefox
-      '&[type=number]': {
-        '-moz-appearance': 'textfield'
-      }
-    }
-  }
-
-  if (isPositiveNumber) {
-    styles['& .MuiInputBase-input'] = {
-      ...styles['& .MuiInputBase-input'],
-      color: activeColor
-    }
-  }
-
-  return styles
-})
-
-const StyledWeightInputBorderBottom = styled("div", {
-  shouldForwardProp: (prop) => prop !== "isPositiveNumber" && prop !== "hasError"
-})(({ isPositiveNumber = false, hasError = false }: { isPositiveNumber: boolean; hasError: boolean }) => {
-  const styles: Record<string, any> = {
-    height: 2,
-    width: 68,
-    backgroundColor: grayColor
-  }
-
-  if (isPositiveNumber) {
-    styles.backgroundColor = activeColor
-  }
-
-  if (hasError) {
-    styles.backgroundColor = errorColor
-  }
-
-  return styles
-})
-
 const sx = {
   weightContainer: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
     padding: '32px 0px',
     gap: '16px'
-  },
-  weightColumn: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    gap: '16px',
-    width: 120
-  },
-  weightLabel: {
-    color: primaryColor,
-    fontSize: '20px',
-    fontStyle: 'normal',
-    fontWeight: 400,
-    lineHeight: '140%',
-    textAlign: 'center',
-    flex: 1
-  },
-  weightValueContainer: {
-    height: 46, // important to keep the height of the card
-    flex: 1,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'column',
-    padding: 0
-  },
-  weightValue: {
-    color: primaryColor,
-    fontSize: '40px',
-    fontStyle: 'normal',
-    fontWeight: 500,
-    lineHeight: '120%',
-  },
-  weightUnit: {
-    color: grayColor,
-    fontSize: '16px',
-    fontStyle: 'normal',
-    fontWeight: 400,
-    lineHeight: '100%',
   },
   reasonLabel: {
     color: primaryColor,
@@ -158,25 +51,6 @@ const sx = {
     fontStyle: 'normal',
     fontWeight: 400,
     lineHeight: '157.1%',
-  },
-  weightInput: {
-    flex: 1,
-    padding: 0,
-    '& .MuiOutlinedInput-notchedOutline': {
-      textAlign: 'center',
-      border: 'none'
-    },
-    '& .MuiInputBase-input': {
-      fontSize: '40px',
-      fontStyle: 'normal',
-      fontWeight: 500,
-      lineHeight: '120%',
-      padding: 0,
-      minWidth: 48,
-      textAlign: 'center',
-      display: 'flex',
-      justifyContent: 'center',
-    }
   },
 };
 
@@ -205,8 +79,8 @@ const SectionField = ({
     setFieldValue(`sections[${sectionIndex}].counterWeighing.reason`, reason.value)
   }
 
-  const handleChangeWeight = (sectionIndex: number) => (event: ChangeEvent<HTMLInputElement>) => {
-    setFieldValue(`sections[${sectionIndex}].counterWeighing.weight`, +event.target.value)
+  const handleChangeWeight = (sectionIndex: number) => (value: number) => {
+    setFieldValue(`sections[${sectionIndex}].counterWeighing.weight`, value)
   }
 
   const handleResetSection = (section: Record<string, any>, sectionIndex: number) => {
@@ -220,50 +94,21 @@ const SectionField = ({
   return (
     <Stack alignItems="center" spacing={16 / 6}>
       {/* ------- top: weight ------- */}
-      <Box sx={sx.weightContainer}>
-        <Stack direction="row" sx={{ gap: "96px" }}>
-          {/* label */}
-          <Box sx={sx.weightColumn}>
-            <Typography sx={sx.weightLabel}>
-              Poids initiale
-            </Typography>
-            <Box sx={sx.weightValueContainer}>
-              <Typography sx={sx.weightValue}>
-                125
-              </Typography>
-            </Box>
-            <Typography sx={sx.weightUnit}>
-              kg
-            </Typography>
-          </Box>
-          {/* value */}
-          <Box sx={sx.weightColumn}>
-            <Typography sx={sx.weightLabel}>
-              Poids de la contre-pesée
-            </Typography>
-            <Box sx={sx.weightValueContainer}>
-              <StyledWeightInput
-                type="number"
-                value={values[sectionIndex]?.counterWeighing?.weight || 0}
-                onChange={handleChangeWeight(sectionIndex)}
-                isPositiveNumber={values[sectionIndex]?.counterWeighing?.weight > 0}
-              />
-              <StyledWeightInputBorderBottom
-                hasError={
-                  // individual section error: when weight is not defined
-                  !!(errors && (errors as any)[sectionIndex]?.counterWeighing?.weight)
-                  // global error: for all sections
-                  || !!hasGlobalError
-                }
-                isPositiveNumber={values[sectionIndex]?.counterWeighing?.weight > 0}
-              />
-            </Box>
-            <Typography sx={sx.weightUnit}>
-              kg
-            </Typography>
-          </Box>
-        </Stack>
-      </Box>
+      <WeightInput
+        value={values[sectionIndex]?.counterWeighing?.weight || 0}
+        onChange={handleChangeWeight(sectionIndex)}
+        label="Poids initiale"
+        valueLabel={125}
+        subtitle="kg"
+        inputLabel="Poids de la contre-pesée"
+        hasError={
+          // individual section error: when weight is not defined
+          !!(errors && (errors as any)[sectionIndex]?.counterWeighing?.weight)
+          // global error: for all sections
+          || !!hasGlobalError
+        }
+        sxRoot={sx.weightContainer}
+      />
       {/* -------- bottom: reason -------- */}
       <Stack spacing={16 / 6} sx={{ alignSelf: "stretch" }}>
         {/* list of reasons */}
